303f98d122c364a39576f825047930ccb20546de
Marcelina Kucięba <marcelinakucieba@wp.pl>
2018-04-06 11:55:40 +0200

    Move audio tags to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+document.querySelector(".sounds").innerHTML = '<audio data-keycode="65" src="sounds/clap.wav"></audio><audio data-keycode="83" src="sounds/hihat.wav"></audio><audio data-keycode="68" src="sounds/kick.wav"></audio><audio data-keycode="70" src="sounds/openhat.wav"></audio><audio data-keycode="71" src="sounds/boom.wav"></audio><audio data-keycode="72" src="sounds/ride.wav"></audio><audio data-keycode="74" src="sounds/snare.wav"></audio><audio data-keycode="75" src="sounds/tom.wav"></audio><audio data-keycode="76" src="sounds/tink.wav"></audio>'
+
 window.addEventListener('keydown', playSound);
 
 function playSound(e) {
